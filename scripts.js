60b611e371d875211b7b742b87a53134e3ae9968
Atumonye James <154873745+Jamestown34@users.noreply.github.com>
2025-07-23 17:57:20 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,38 @@
+// External Script: https://valiotti.com/wp-includes/js/jquery/jquery.min.js
+// External Script: https://valiotti.com/wp-includes/js/jquery/jquery-migrate.min.js
+// External Script: https://valiotti.com/wp-content/plugins/contact-form-7/includes/swv/js/index.js
+// External Script: https://valiotti.com/wp-content/plugins/contact-form-7/includes/js/index.js
+// External Script: https://valiotti.com/wp-content/plugins/la-sentinelle-antispam/spamfilters/js/la-sentinelle-frontend.js
+// External Script: https://valiotti.com/wp-content/themes/valiotti_theme/assets/js/scripts.js
+// External Script: https://valiotti.com/wp-content/themes/valiotti_theme/assets/js/swiper.min.js
+window.addEventListener('load', function() {
+            setTimeout(function() {
+                (function(w, d, s, l, i) {
+                    w[l] = w[l] || [];
+                    w[l].push({
+                        'gtm.start': new Date().getTime(),
+                        event: 'gtm.js'
+                    });
+                    var f = d.getElementsByTagName(s)[0],
+                        j = d.createElement(s),
+                        dl = l != 'dataLayer' ? '&l=' + l : '';
+                    j.async = true;
+                    j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+                    f.parentNode.insertBefore(j, f);
+                })(window, document, 'script', 'dataLayer', 'GTM-WJ3DGCP');
+            }, 2000);
+        });
+! function(c, h, i, m, p) {
+            m = c.createElement(h), p = c.getElementsByTagName(h)[0], m.async = 1, m.src = i, p.parentNode.insertBefore(m, p)
+        }(document, "script", "https://chimpstatic.com/mcjs-connected/js/users/78283b059df394b8289dd8b77/35abb6fc419a40720d3a2ea4f.js");
+{"prefetch":[{"source":"document","where":{"and":[{"href_matches":"\/*"},{"not":{"href_matches":["\/wp-*.php","\/wp-admin\/*","\/wp-content\/uploads\/*","\/wp-content\/*","\/wp-content\/plugins\/*","\/wp-content\/themes\/valiotti_theme\/*","\/*\\?(.+)"]}},{"not":{"selector_matches":"a[rel~=\"nofollow\"]"}},{"not":{"selector_matches":".no-prefetch, .no-prefetch a"}}]},"eagerness":"conservative"}]}
+/* <![CDATA[ */
+var wpcf7 = {"api":{"root":"https:\/\/valiotti.com\/wp-json\/","namespace":"contact-form-7\/v1"}};
+/* ]]> */
+/* <![CDATA[ */
+var la_sentinelle_frontend_script = {"ajaxurl":"https:\/\/valiotti.com\/wp-admin\/admin-ajax.php","ajax2":"la_sentinelle_88f2391a47d0f3afd254854591428319","ajax3":"la_sentinelle_cb68545722c18882a9eb2b51daf332e0","honeypot":"la_sentinelle_fe6e4fb4e9e55b650c1ea38158c6536e","honeypot2":"la_sentinelle_cc14236b103adee018b9146f74ee8d52","timeout":"la_sentinelle_535e441a9c17517c948adbec4754ea65","timeout2":"la_sentinelle_e4c6c2bdd0230546edd265c317726a51","webgl":"la_sentinelle_c3e79fcd68252805642d1c4327341d30","webgl2":"la_sentinelle_34ce002eb425f4f94c08e3c63aaad289","webgl3":"la_sentinelle_a98ae1f6312e7d0bb9067ddea3dfaa55"};
+/* ]]> */
+document.addEventListener('DOMContentLoaded', function() {
+			var el = document.getElementById('page_url');
+			if(el){ el.value = window.location.href; }
+		});
